85837fb0d3132f14984d80e3fed9f1125d9be1dd
Anmol <anmolgaur26@gmail.com>
2023-10-12 23:52:36 +0530

    responsive ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <>
     <div className='h-screen flex justify-center items-center'>
       <h1 className='text-10xl text-center mb-24 font-bold absolute '>Hello&nbsp;&nbsp; &nbsp; &nbsp;Senpai!</h1>
-      <img src={Goku}  className='h-[80%] relative right-' />
+      <img src={Goku}  className='h-[80%] relative right-14' />
     </div>
     <Productlist/>
     </>

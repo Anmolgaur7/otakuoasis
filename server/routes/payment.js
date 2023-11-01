9231a2e9e36d5ed1b12296f2479bfb96903a153e
Anmol <anmolgaur26@gmail.com>
2023-11-01 17:20:04 +0530

    Payments  started

---
+++ server/routes/payment.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+const stripe = require('stripe')('pk_test_51O7ctDSHY56XZU6lLSpO2HWofdo1xdXFmcoeRFp1faUWYJgXcNuLsjReumhpdBEAgq29AmCnSj5F5DXOg6IFE2h700o4mrKYp3')
+
